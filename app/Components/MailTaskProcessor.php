7c869a13cee2d226bbe0ee7a1fa5520c4854220c
Vladimir Zhernakov <Zhernakov-vladimir@ukr.net>
2016-11-10 12:28:43 +0200

    MailtaskProcessor fix

--- app/Components/MailTaskProcessor.php
+++ app/Components/MailTaskProcessor.php
@@ -15,10 +15,10 @@ class MailTaskProcessor
 {
     public static function run()
     {
-       $mail =  Mailtask::untouched()->first();
-        if ($mail) {
+        
+        while($mail =  Mailtask::untouched()->first()) {
             $mail->TouchMessage();
-            
+
             while($mail =  Mailtask::touched()->first()) {$mail->send();}
         }
         
